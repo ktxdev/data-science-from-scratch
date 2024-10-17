e603a05f03a5eb9642ec991b4a77c6316ada1365
Sean Huvaya <sean.ktxdev@gmail.com>
2024-10-17 03:25:27 -0400

    Implemented vector subtraction

--- 04-linear_algebra.py
+++ 04-linear_algebra.py
@@ -8,3 +8,10 @@ def add(v: Vector, w: Vector) -> Vector:
     return [v_i + w_i for v_i, w_i in zip(v, w)]
 
 assert add([1,2,3],[4,5,6]) == [5,7,9]
+
+def subtract(v: Vector, w: Vector) -> Vector:
+    """Subtracts corresponding elements"""
+    assert len(v) == len(w), "Vectors must be the same length"
+    return [v_i - w_i for v_i, w_i in zip(v, w)]
+
+assert subtract([5,7,9],[4,5,6]) == [1,2,3]
